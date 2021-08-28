3d8bc4695e79a1eade6b19040bfc45a0d7985fa2
Liassid <53168424+Liassid@users.noreply.github.com>
2021-08-28 12:11:36 +0500

    Update ru.ts

--- langs/ru.ts
+++ langs/ru.ts
@@ -29,7 +29,7 @@ const RU: Translation = {
     pardonSuccess: "Игрок успешно разбанен!",
     delete: "Удалить",
     deleteSuccess: "Предупреждение/бан успешно удален!",
-    confirmTitle: "Are you sure?",
+    confirmTitle: "Вы уверены?",
     confirmMessage: "Подтвердите, что вы хотите выполнить данное действие.",
     confirm: "Подтвердить",
     cancel: "Отмена",
